dab4edba199665ecd6fc41b0c386cd680271aefb
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-19 19:26:42 -0400

    add data file

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Morning lecture', done: true, _id: 125223},
+  {text: 'Lunch break', done: false, _id: 127904},
+  {text: 'Afternoon lecture', done: false, _id: 139608},
+]
+
+export { 
+  students
+}
