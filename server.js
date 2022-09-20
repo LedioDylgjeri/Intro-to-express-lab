7e3feebcf56db91ccc62a10b70bd674c3d92598f
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-20 18:18:40 -0400

    create views folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // import modules
 
-import express  from "express";
+import express from "express";
 import { students } from "./data/students-data.js";
 
 // create express app
@@ -15,14 +15,34 @@ app.set('view engine', 'ejs')
 
 // mount routes
 
-app.get('/home', function(req, res) {
-  res.render('home')
-})
+// app.get('/', function(req, res) {
+//   res.redirect('/students')
+// })
+
+// app.get('/students', function(req, res) {
+//   res.render('students')
+// })
 
 app.get('/students', function(req, res) {
   res.render('students/index', {
     students: students
   })
+})
+
+app.get('/', function(req, res){
+  res.send('<h1>hello</h1>')
+})
+
+app.get('/waffles', function(req, res){
+  res.redirect('/home')
+})
+
+app.get('/pandas', function(req, res){
+  res.send('<h2>Pandas are dope</h2>')
+})
+
+app.get('/home', function(req, res){
+  res.render('home')
 })
 
 // tell the app to listen on port 3000
